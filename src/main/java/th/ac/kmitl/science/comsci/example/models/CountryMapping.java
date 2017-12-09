f9aa1ef419b4b1726b986279fd27c6aa6113f1a7
Phatipan Suwanich <58050315@kmitl.ac.th>
2017-12-09 17:36:34 +0700

    feature: create mapping for Country

---
+++ src/main/java/th/ac/kmitl/science/comsci/example/models/CountryMapping.java
@@ -0,0 +1,30 @@
+package th.ac.kmitl.science.comsci.example.utilities;
+
+import th.ac.kmitl.science.comsci.example.models.Mapping;
+
+public class CountryMapping implements Mapping {
+    
+    @Override
+    public int mapper(String name) throws Exception {
+        switch(name){
+            case "Thailand"                 : return 1;
+            case "Laos"                     : return 2;
+            case "Brazil"                   : return 3;
+            case "Argentina"                : return 4;
+            case "China"                    : return 5;
+            case "England"                  : return 6;
+            case "France"                   : return 7;
+            case "Spain"                    : return 8;
+            case "Singapore"                : return 9;
+            case "Mexico"                   : return 10;
+            case "Canada"                   : return 11;
+            case "India"                    : return 12;
+            case "United States of America" : return 13;
+            case "USA"                      : return 13;
+            case "Japan"                    : return 14;
+            case "North Korea"              : return 15;
+            case "South Korea"              : return 16;
+            default : throw new IllegalArgumentException("MappingNotFoundException");
+        }
+    }
+}
